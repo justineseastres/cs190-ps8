b9761e4ebbf6c4c3a7f9146bf267a646f8d7c725
Justine Seastres <Justine@Justines-MacBook-Air.local>
2016-04-26 09:44:20 -0700

    solution to problem 8

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -43,9 +43,18 @@ struct LocationTrack {
     
     var length: CLLocationDistance {
         // this function should sum up all the distances between the locations in the track
-        return 1000.0 // right now it just returns 1000 (in meters, which is one kilometer).
-    }
-    
+        var summ: Double = 0
+        if locations.count == 0{
+            return summ
+        }
+        //for loop that determines distance between certain number of locations
+        //adds up distance between each point
+        //sums it up
+        for i in 0..<(locations.count-1){
+            summ += locations[i].distanceFromLocation(locations[i+1])
+        }
+            return summ
+        }
 }
 
 
@@ -66,11 +75,37 @@ class LocationTrackTestSuite: XCTestCase {
         XCTAssertEqual(expectedResult, onePointTrack.length, "Single point track should have zero length.")
     }
     
+    func testLengthOfGPSCorrect() {
+        let sanFrancisco = CLLocation(latitude: 37.7749, longitude: 122.4194)
+        let moraga = CLLocation(latitude: 37.8349, longitude: 122.1297)
+        let twoPointTrack = LocationTrack(locations: [sanFrancisco, moraga])
+        //boolean value that makes sure that the distance is within the defined values of 15000 and 30000
+        let equalResult = twoPointTrack.length>15000 && twoPointTrack.length<30000
+        XCTAssertTrue(equalResult, "Calculated length not equal to the expected length")
+        
+    }
+        
+    }
     
-}
+    
+    
+    
+    //        let sanFrancisco = CLLocation(latitude: 37.7749, longitude: 122.4194)
+    //        let firstPoint = LocationTrack(locations: [sanFrancisco, oakland])
+    //        let expectedResult: CLLocationDistance = 31500
+    //        XCTAssertEqual(expectedResult, finalDistance.length, "35100 meters")
+    //        let moraga = CLLocation(latitude: 37.8349, longitude: 122.1297)
+    //        let oakland = CLLocation(latitude: 37.8044, longitude: 122.2711)
+    //
+    //        var firstDistance = distanceFromLocation(moraga: CLLocation)
+    //        var secondDistance = distanceFromLocation(oakland: CLLocation)
+    //        var finalDistance = firstDistance + secondDistance
+    
+
 /*:
  The last bit of arcana is necessary to support the execution of unit tests in a playground, but isn't documented in [Apple's XCTest Library]( https://github.com/apple/swift-corelibs-xctest ). I gratefully acknowledge Stuart Sharpe for sharing it in his blog post, [TDD in Swift Playgrounds]( http://initwithstyle.net/2015/11/tdd-in-swift-playgrounds/ ). */
-class PlaygroundTestObserver : NSObject, XCTestObservation {
+
+    class PlaygroundTestObserver : NSObject, XCTestObservation {
     @objc func testCase(testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
         print("Test failed on line \(lineNumber): \(description)")
     }
